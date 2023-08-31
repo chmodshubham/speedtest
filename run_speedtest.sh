74103ca9ff56fd495a18001e88b76b1247a020f1
Shubham Kumar <Shubham.Kumar@ramanujan.du.ac.in>
2023-08-31 11:04:51 +0530

    Update run_speedtest.sh

--- run_speedtest.sh
+++ run_speedtest.sh
@@ -3,8 +3,8 @@
 # Function to run the script itself
 run_script() {
     while true; do
-        # Send "YES" input to speedtest command for license acceptance
-        echo "YES" | speedtest
+        # Send "YES" input to speedtest command for license acceptance and highlight the "Speedtest by Ookla" line in light blue
+        echo "YES" | speedtest | awk '/Speedtest by Ookla/ {print "\033[1;94m" $0 "\033[0m"; next} {print}'
 
         # Check the exit status of the speedtest command
         if [ $? -ne 0 ]; then
